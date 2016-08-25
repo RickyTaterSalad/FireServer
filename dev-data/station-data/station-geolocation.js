443fd6216d0577d07d51db17a7b0562f86fc904d
RickyTaterSalad <rich5127@esri.com>
2016-08-25 12:12:49 -0700

    added station geolocation

---
+++ dev-data/station-data/station-geolocation.js
@@ -0,0 +1,535 @@
+module.exports = {
+    geolocationLookupByStationId: {
+        "1": {
+            "address": "2230 Pasadena Avenue Los Angeles",
+            "latitude": 34.075375,
+            "longitude": 118.217721
+        },
+        "2": {
+            "address": "1962 East Cesar Chavez Avenue Los Angeles",
+            "latitude": 34.049053,
+            "longitude": 118.213501
+        },
+        "3": {
+            "address": "108 North Fremont Avenue Los Angeles",
+            "latitude": 34.058502,
+            "longitude": 118.252391
+        },
+        "4": {
+            "address": "450 East Temple Street Los Angeles",
+            "latitude": 34.050007,
+            "longitude": 118.235869
+        },
+        "5": {
+            "address": "8900 South Emerson Avenue Los Angeles",
+            "latitude": 33.955708,
+            "longitude": 118.404048
+        },
+        "6": {
+            "address": "326 North Virgil Avenue Los Angeles",
+            "latitude": 34.077524,
+            "longitude": 118.28639
+        },
+        "7": {
+            "address": "14630 Plummer Street Los Angeles",
+            "latitude": 34.24242,
+            "longitude": 118.451577
+        },
+        "8": {
+            "address": "11351 Tampa Avenue Los Angeles",
+            "latitude": 34.277304,
+            "longitude": 118.559708
+        },
+        "9": {
+            "address": "430 East 7th Street Los Angeles",
+            "latitude": 34.041134,
+            "longitude": 118.247889
+        },
+        "10": {
+            "address": "1335 South Olive Street Los Angeles",
+            "latitude": 34.03745,
+            "longitude": 118.263953
+        },
+        "11": {
+            "address": "1819 West 7th Street Los Angeles",
+            "latitude": 34.055484,
+            "longitude": 118.273924
+        },
+        "12": {
+            "address": "5921 North Figueroa Street Los Angeles",
+            "latitude": 34.111214,
+            "longitude": 118.190288
+        },
+        "13": {
+            "address": "2401 West Pico Boulevard Los Angeles",
+            "latitude": 34.047515,
+            "longitude": 118.289392
+        },
+        "14": {
+            "address": "3401 South Central Avenue Los Angeles",
+            "latitude": 34.012553,
+            "longitude": 118.256906
+        },
+        "15": {
+            "address": "915 West Jefferson Boulevard Los Angeles",
+            "latitude": 34.024531,
+            "longitude": 118.285061
+        },
+        "16": {
+            "address": "2011 North Eastern Avenue Los Angeles",
+            "latitude": 34.066279,
+            "longitude": 118.180612
+        },
+        "17": {
+            "address": "1601 South Santa Fe Avenue Los Angeles",
+            "latitude": 34.025273,
+            "longitude": 118.230287
+        },
+        "18": {
+            "address": "12050 Balboa Boulevard Los Angeles",
+            "latitude": 34.28972,
+            "longitude": 118.501276
+        },
+        "19": {
+            "address": "12229 West Sunset Boulevard Los Angeles",
+            "latitude": 34.058533,
+            "longitude": 118.479579
+        },
+        "20": {
+            "address": "2144 West Sunset Boulevard Los Angeles",
+            "latitude": 34.077137,
+            "longitude": 118.265344
+        },
+        "21": {
+            "address": "1192 East 51st Street Los Angeles",
+            "latitude": 33.996933,
+            "longitude": 118.254639
+        },
+        "23": {
+            "address": "17281 Sunset Boulevard Los Angeles",
+            "latitude": 34.042263,
+            "longitude": 118.55439
+        },
+        "24": {
+            "address": "9411 Wentworth Street Los Angeles",
+            "latitude": 34.263803,
+            "longitude": 118.336621
+        },
+        "25": {
+            "address": "2927 Whittier Boulevard Los Angeles",
+            "latitude": 34.031079,
+            "longitude": 118.207712
+        },
+        "26": {
+            "address": "2009 South Western Avenue Los Angeles",
+            "latitude": 34.038401,
+            "longitude": 118.309418
+        },
+        "27": {
+            "address": "1327 North Cole Avenue Los Angeles",
+            "latitude": 34.095531,
+            "longitude": 118.330159
+        },
+        "28": {
+            "address": "11641 Corbin Avenue Los Angeles",
+            "latitude": 34.281149,
+            "longitude": 118.569679
+        },
+        "29": {
+            "address": "4029 West Wilshire Boulevard Los Angeles",
+            "latitude": 34.06209,
+            "longitude": 118.315655
+        },
+        "33": {
+            "address": "6406 South Main Street Los Angeles",
+            "latitude": 33.981243,
+            "longitude": 118.273674
+        },
+        "34": {
+            "address": "3661 7th Avenue Los Angeles",
+            "latitude": 34.021263,
+            "longitude": 118.325173
+        },
+        "35": {
+            "address": "1601 North Hillhurst Avenue Los Angeles",
+            "latitude": 34.100263,
+            "longitude": 118.287729
+        },
+        "36": {
+            "address": "1005 North Gaffey Street Los Angeles",
+            "latitude": 33.753118,
+            "longitude": 118.293324
+        },
+        "37": {
+            "address": "1090 Veteran Avenue Los Angeles",
+            "latitude": 34.059791,
+            "longitude": 118.448947
+        },
+        "38": {
+            "address": "124 East I Street Los Angeles",
+            "latitude": 33.780971,
+            "longitude": 118.261741
+        },
+        "39": {
+            "address": "14415 Sylvan Street Los Angeles",
+            "latitude": 34.184898,
+            "longitude": 118.447144
+        },
+        "40": {
+            "address": "330 Ferry Street Los Angeles",
+            "latitude": 33.746699,
+            "longitude": 118.26183
+        },
+        "41": {
+            "address": "1439 North Gardner Street Los Angeles",
+            "latitude": 34.097284,
+            "longitude": 118.353142
+        },
+        "42": {
+            "address": "2021 Colorado Boulevard Los Angeles",
+            "latitude": 34.13957,
+            "longitude": 118.210929
+        },
+        "43": {
+            "address": "3690 Motor Avenue Los Angeles",
+            "latitude": 34.022976,
+            "longitude": 118.405531
+        },
+        "44": {
+            "address": "1410 Cypress Avenue Los Angeles",
+            "latitude": 34.096954,
+            "longitude": 118.231106
+        },
+        "46": {
+            "address": "4370 South Hoover Street Los Angeles",
+            "latitude": 34.00405,
+            "longitude": 118.286816
+        },
+        "47": {
+            "address": "4575 Huntington Drive South Los Angeles",
+            "latitude": 34.085404,
+            "longitude": 118.186937
+        },
+        "48": {
+            "address": "1601 South Grand Avenue Los Angeles",
+            "latitude": 34.035746,
+            "longitude": 118.266371
+        },
+        "49": {
+            "address": "400 Yacht Street, Berth 194 Los Angeles",
+            "latitude": 33.759687,
+            "longitude": 118.262094
+        },
+        "50": {
+            "address": "3036 Fletcher Drive Los Angeles",
+            "latitude": 34.115393,
+            "longitude": 118.244624
+        },
+        "51": {
+            "address": "10435 Sepulveda Boulevard Los Angeles",
+            "latitude": 34.260136,
+            "longitude": 118.467507
+        },
+        "52": {
+            "address": "4957 Melrose Avenue Los Angeles",
+            "latitude": 34.083795,
+            "longitude": 118.308069
+        },
+        "55": {
+            "address": "4455 East York Boulevard Los Angeles",
+            "latitude": 34.12403,
+            "longitude": 118.218521
+        },
+        "56": {
+            "address": "2759 Rowena Avenue Los Angeles",
+            "latitude": 34.107702,
+            "longitude": 118.263889
+        },
+        "57": {
+            "address": "7800 South Vermont Avenue Los Angeles",
+            "latitude": 33.968565,
+            "longitude": 118.291157
+        },
+        "58": {
+            "address": "1556 South Robertson Boulevard Los Angeles",
+            "latitude": 34.050707,
+            "longitude": 118.38395
+        },
+        "59": {
+            "address": "11505 Olympic Boulevard Los Angeles",
+            "latitude": 34.036609,
+            "longitude": 118.444457
+        },
+        "60": {
+            "address": "5320 Tujunga Avenue Los Angeles",
+            "latitude": 34.167765,
+            "longitude": 118.378636
+        },
+        "61": {
+            "address": "5821 West 3rd Street Los Angeles",
+            "latitude": 34.069411,
+            "longitude": 118.347118
+        },
+        "62": {
+            "address": "11970 Venice Avenue Los Angeles",
+            "latitude": 34.005925,
+            "longitude": 118.428312
+        },
+        "63": {
+            "address": "1930 Shell Avenue Los Angeles",
+            "latitude": 33.991348,
+            "longitude": 118.459188
+        },
+        "64": {
+            "address": "10811 South Main Street Los Angeles",
+            "latitude": 33.937822,
+            "longitude": 118.274254
+        },
+        "65": {
+            "address": "1801 East Century Boulevard Los Angeles",
+            "latitude": 33.94599,
+            "longitude": 118.24025
+        },
+        "66": {
+            "address": "1909 West Slauson Boulevard Los Angeles",
+            "latitude": 33.989219,
+            "longitude": 118.311526
+        },
+        "67": {
+            "address": "5451 Playa Vista Drive Los Angeles",
+            "latitude": 33.974336,
+            "longitude": 118.428536
+        },
+        "68": {
+            "address": "5023 Washington Boulevard Los Angeles",
+            "latitude": 34.040824,
+            "longitude": 118.349646
+        },
+        "69": {
+            "address": "15045 Sunset Boulevard Los Angeles",
+            "latitude": 34.04507,
+            "longitude": 118.522471
+        },
+        "70": {
+            "address": "9861 Reseda Bouelvard Los Angeles",
+            "latitude": 34.249746,
+            "longitude": 118.53653
+        },
+        "71": {
+            "address": "107 South Beverly Glen Boulevard Los Angeles",
+            "latitude": 34.080296,
+            "longitude": 118.435331
+        },
+        "72": {
+            "address": "6811 De Soto Avenue Los Angeles",
+            "latitude": 34.193945,
+            "longitude": 118.588567
+        },
+        "73": {
+            "address": "7419 Reseda Boulevard Los Angeles",
+            "latitude": 34.205409,
+            "longitude": 118.536427
+        },
+        "74": {
+            "address": "7777 Foothill Boulevard Los Angeles",
+            "latitude": 34.258439,
+            "longitude": 118.301504
+        },
+        "75": {
+            "address": "15345 San Fernando Mission Los Angeles",
+            "latitude": 34.27252,
+            "longitude": 118.466768
+        },
+        "76": {
+            "address": "3111 North Cahuenga Boulevard Los Angeles",
+            "latitude": 34.111456,
+            "longitude": 118.334622
+        },
+        "77": {
+            "address": "9224 Sunland Boulevard Los Angeles",
+            "latitude": 34.236294,
+            "longitude": 118.366933
+        },
+        "78": {
+            "address": "4041 Whitsett Avenue Los Angeles",
+            "latitude": 34.144369,
+            "longitude": 118.40565
+        },
+        "79": {
+            "address": "18030 South Vermont Avenue Los Angeles",
+            "latitude": 33.8676,
+            "longitude": 118.290295
+        },
+        "80": {
+            "address": "7250 World Way  Los Angeles",
+            "latitude": 33.940507,
+            "longitude": 118.419492
+        },
+        "81": {
+            "address": "14355 Arminta Street Los Angeles",
+            "latitude": 34.213127,
+            "longitude": 118.444724
+        },
+        "82": {
+            "address": "5769 Hollywood Boulevard Los Angeles",
+            "latitude": 34.102017,
+            "longitude": 118.315506
+        },
+        "83": {
+            "address": "4960 Balboa Boulevard Los Angeles",
+            "latitude": 34.161107,
+            "longitude": 118.500678
+        },
+        "84": {
+            "address": "21050 Burbank Boulevard Los Angeles",
+            "latitude": 34.172908,
+            "longitude": 118.59162
+        },
+        "85": {
+            "address": "1331 West 253rd Street Los Angeles",
+            "latitude": 33.794988,
+            "longitude": 118.300211
+        },
+        "86": {
+            "address": "4305 Vineland Avenue Los Angeles",
+            "latitude": 34.148469,
+            "longitude": 118.370744
+        },
+        "87": {
+            "address": "10124 Balboa Boulevard Los Angeles",
+            "latitude": 34.254638,
+            "longitude": 118.501857
+        },
+        "88": {
+            "address": "5101 North Sepulveda Boulevard Los Angeles",
+            "latitude": 34.130038,
+            "longitude": 118.480582
+        },
+        "89": {
+            "address": "7063 Laurel Canyon Boulevard Los Angeles",
+            "latitude": 34.199207,
+            "longitude": 118.397069
+        },
+        "90": {
+            "address": "7921 Woodley Avenue Los Angeles",
+            "latitude": 34.214713,
+            "longitude": 118.484237
+        },
+        "91": {
+            "address": "14430 Polk Street Los Angeles",
+            "latitude": 34.309562,
+            "longitude": 118.447347
+        },
+        "92": {
+            "address": "10556 West Pico Boulevard Los Angeles",
+            "latitude": 34.044067,
+            "longitude": 118.420412
+        },
+        "93": {
+            "address": "19059 Ventura Boulevard Los Angeles",
+            "latitude": 34.172919,
+            "longitude": 118.549238
+        },
+        "94": {
+            "address": "4470 Coliseum Street Los Angeles",
+            "latitude": 34.017527,
+            "longitude": 118.345541
+        },
+        "95": {
+            "address": "10010 International Road Los Angeles",
+            "latitude": 33.945189,
+            "longitude": 118.381187
+        },
+        "96": {
+            "address": "21800 Marilla Street Los Angeles",
+            "latitude": 34.247278,
+            "longitude": 118.603552
+        },
+        "97": {
+            "address": "8021 Mulholland Drive Los Angeles",
+            "latitude": 34.126202,
+            "longitude": 118.374532
+        },
+        "98": {
+            "address": "13035 Van Nuys Boulevard Los Angeles",
+            "latitude": 34.26997,
+            "longitude": 118.417306
+        },
+        "99": {
+            "address": "14145 Mulholland Drive Los Angeles",
+            "latitude": 34.131796,
+            "longitude": 118.440355
+        },
+        "100": {
+            "address": "6751 Louise Avenue Los Angeles",
+            "latitude": 34.193388,
+            "longitude": 118.509968
+        },
+        "101": {
+            "address": "1414 25th Street Los ",
+            "latitude": 33.721943,
+            "longitude": 118.310277
+        },
+        "102": {
+            "address": "13200 Burbank Boulevard Los Angeles",
+            "latitude": 34.171835,
+            "longitude": 118.420709
+        },
+        "103": {
+            "address": "18143 Parthenia Street Los Angeles",
+            "latitude": 34.228993,
+            "longitude": 118.528824
+        },
+        "104": {
+            "address": "8349 Winnetka Avenue Los Angeles",
+            "latitude": 34.221639,
+            "longitude": 118.571438
+        },
+        "105": {
+            "address": "6345 Fallbrook Avenue Los Angeles",
+            "latitude": 34.185742,
+            "longitude": 118.623804
+        },
+        "106": {
+            "address": "23004 Roscoe Boulevard Los Angeles",
+            "latitude": 34.219017,
+            "longitude": 118.628074
+        },
+        "107": {
+            "address": "20225 Devonshire Street Los Angeles",
+            "latitude": 34.257627,
+            "longitude": 118.573661
+        },
+        "108": {
+            "address": "12520 Mulholland Drive Los Angeles",
+            "latitude": 34.12701,
+            "longitude": 118.406239
+        },
+        "109": {
+            "address": "16500 Mulholland Drive Los Angeles",
+            "latitude": 34.13073,
+            "longitude": 118.492115
+        },
+        "110": {
+            "address": "2945 Miner Street, Berth 44-A  Los Angeles",
+            "latitude": 33.761954,
+            "longitude": 118.262952
+        },
+        "111": {
+            "address": "954 South Seaside Avenue Berth 260 Los Angeles",
+            "latitude": 33.759578,
+            "longitude": 118.262087
+        },
+        "112": {
+            "address": "444 South Harbor Boulevard Berth 86 Los Angeles",
+            "latitude": 33.761954,
+            "longitude": 118.262952
+        },
+        "114": {
+            "address": "16617 Arminta Street Los Angeles",
+            "latitude": 34.215114,
+            "longitude": 118.494962
+        }
+    }
+
+}
